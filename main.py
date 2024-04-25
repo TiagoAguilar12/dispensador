2ee9d524169ad79ff03de694e0955231991ea7a9
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-04-25 17:22:58 -0500

    fk

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ def main():
     pi.write(motor1_en_pin, 1)  # GPIO.HIGH
     pi.write(motor2_en_pin, 1)  # GPIO.HIGH
 
-    control_motor(motor1_pwm_pin, 30, 'forward')
-    control_motor(motor2_pwm_pin, 30, 'forward')
+    control_motor(motor1_pwm_pin, 40, 'forward')
+    control_motor(motor2_pwm_pin, 40, 'forward')
 
     time.sleep(5)
     pi.set_PWM_dutycycle(motor1_pwm_pin, 0)  # Detener motor 1
