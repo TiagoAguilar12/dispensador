79ca5da5492565a05319082de156d63589d406ec
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-04-30 17:01:57 -0500

    test correction

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # -- coding: utf-8 --
 import time
 import pigpio
-# Configuración de pines
+
 motor1_pwm_pin = 12
 motor1_dir_pin = 24
 motor1_en_pin = 22
@@ -10,7 +10,7 @@ motor2_dir_pin = 25
 motor2_en_pin = 23
 pinA = 17
 pinB = 18
-# Inicialización de la conexión con pigpio
+
 pi = pigpio.pi()
 pi.set_mode(pinA, pigpio.INPUT)
 
@@ -32,14 +32,14 @@ def count_rpm(gpio, level, tick):
 cb = pi.callback(pinA, pigpio.RISING_EDGE, count_rpm)
 
 # Función para controlar la velocidad y dirección de los motores
-def control_motor(pin_pwm, speed_percent, direction):
+def control_motor(pin_pwm, pin_dir, speed_percent, direction):
     duty_cycle = int(speed_percent * 255 / 100)  # Convertir el porcentaje de velocidad a ciclo de trabajo (0-255)
     pi.set_PWM_dutycycle(pin_pwm, duty_cycle)
 
     if direction == 'forward':
-        pi.write(motor1_dir_pin, 1)  # GPIO.HIGH
+        pi.write(pin_dir, 1)  # Establecer dirección hacia adelante
     elif direction == 'backward':
-        pi.write(motor1_dir_pin, 0)  # GPIO.LOW
+        pi.write(pin_dir, 0)  # Establecer dirección hacia atrás
     else:
         raise ValueError("Dirección no válida. Usa 'forward' o 'backward'.")
 
@@ -48,8 +48,8 @@ def main():
     pi.write(motor1_en_pin, 1)  # GPIO.HIGH
     pi.write(motor2_en_pin, 1)  # GPIO.HIGH
 
-    control_motor(motor1_pwm_pin, 100, 'forward')
-    control_motor(motor2_pwm_pin, 100, 'forward')
+    control_motor(motor1_pwm_pin, motor1_dir_pin, 100, 'forward')
+    control_motor(motor2_pwm_pin, motor2_dir_pin, 100, 'forward')
 
     try:
         while True:
@@ -73,4 +73,5 @@ def main():
 
         pi.stop()
         print('Movimiento de los motores completado.')
+
 main()
