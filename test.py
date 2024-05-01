4740db1d2dc9dd2b209cf18cdcd64f92572f6d45
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-04-30 19:12:02 -0500

    gf

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-# -- coding: utf-8 --
 import time
 import pigpio
 
@@ -15,7 +14,7 @@ pi = pigpio.pi()
 pi.set_mode(pinA, pigpio.INPUT)
 
 global rpm_count  # Declarar rpm_count como global
-global rpm_count1  # Declarar rpm_count como global
+global rpm_count1  # Declarar rpm_count1 como global
 
 rpm_count = 0
 rpm_count1 = 0
@@ -32,6 +31,7 @@ def count_rpmA(gpio, level, tick):
         rpm_count += 1
 
     last_state = state
+
 def count_rpmB(gpio, level, tick):
     global rpm_count1
     global last_state
@@ -44,7 +44,7 @@ def count_rpmB(gpio, level, tick):
 
 cb = pi.callback(pinA, pigpio.EITHER_EDGE, count_rpmA)
 cb = pi.callback(pinB, pigpio.EITHER_EDGE, count_rpmB)
-suma=0
+suma = 0
 
 # Función para controlar la velocidad y dirección de los motores
 def control_motor(pin_pwm, pin_dir, speed_percent, direction):
@@ -60,6 +60,10 @@ def control_motor(pin_pwm, pin_dir, speed_percent, direction):
 
 def main():
     global rpm_count  # Declarar rpm_count como global dentro de main
+    global rpm_count1  # Declarar rpm_count1 como global dentro de main
+
+    rpm_count = 0
+    rpm_count1 = 0
 
     # Configurar pines de habilitación (enable) de los motores
     pi.write(motor1_en_pin, 1)  # GPIO.HIGH
@@ -67,25 +71,26 @@ def main():
 
     control_motor(motor1_pwm_pin, motor1_dir_pin, 50, 'forward')
     control_motor(motor2_pwm_pin, motor2_dir_pin, 50, 'forward')
-    start_time=time.time()
+    start_time = time.time()
 
-    while time.time()-start_time<= 20:  # Ejemplo: Ejecutar durante 20 segundos
-            suma= rpm_count1 + rpm_count
-            start_time1 = time.time()
-            time.sleep(1)  # Esperar 1 segundo
-            end_time = time.time()
-            time_elapsed = end_time - start_time1
-            print("contflag: " + str(suma) + "time: " + str(time_elapsed))
-            rpm = (suma / 64) / time_elapsed  # Calcular las RPM
-            SIU= (rpm/19)*60
-            print("RPM: {:.2f}".format(rpm))
-            print('siu: {:.2f}'.format(SIU))
+    while time.time() - start_time <= 20:  # Ejemplo: Ejecutar durante 20 segundos
+        suma = rpm_count1 + rpm_count
+        start_time1 = time.time()
+        time.sleep(1)  # Esperar 1 segundo
+        end_time = time.time()
+        time_elapsed = end_time - start_time1
+        print("contflag: " + str(suma) + "time: " + str(time_elapsed))
+        rpm = (suma / 64) / time_elapsed  # Calcular las RPM
+        SIU = (rpm / 19) * 60
+        print("RPM: {:.2f}".format(rpm))
+        print('siu: {:.2f}'.format(SIU))
 
-            suma = 0
-            rpm_count=0
-            rpm_count1=0
-            rpm=0
-            SIU=0
+        suma = 0
+        rpm_count = 0
+        rpm_count1 = 0
+        rpm = 0
+        SIU = 0
+
     cb.cancel()
     time.sleep(5)
     pi.set_PWM_dutycycle(motor1_pwm_pin, 0)  # Detener motor 1
