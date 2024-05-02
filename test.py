4f7104de2255268bd290f33af749b8849f4070a5
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-05-02 12:54:40 -0500

    3

--- test.py
+++ test.py
@@ -6,17 +6,17 @@ motor1_pwm_pin = 12
 motor1_dir_pin = 24
 motor1_en_pin = 22
 motor1_encoder_pinA = 18  # Pin del encoder del motor 1 para la señal A
-motor1_encoder_pinB = 19  # Pin del encoder del motor 1 para la señal B
+
 
 motor2_pwm_pin = 13
 motor2_dir_pin = 25
 motor2_en_pin = 23
 motor2_encoder_pinA = 20  # Pin del encoder del motor 2 para la señal A
-motor2_encoder_pinB = 27  # Pin del encoder del motor 2 para la señal B
+
 
 pi = pigpio.pi()
 
-encoder_counts_per_rev = 64  # Número de cuentas por revolución del encoder
+encoder_counts_per_rev = 32  # Número de cuentas por revolución del encoder
 motor_resolution = 19  # Resolución del motor (19:1)
 
 motor1_encoder_count = 0
@@ -40,13 +40,6 @@ def encoder_callback1A(gpio, level, tick):
 def encoder_callback2A(gpio, level, tick):
     global motor2_encoder_count
     motor2_encoder_count += 1
-def encoder_callback1B(gpio, level, tick):
-    global motor1_encoder_count
-    motor1_encoder_count += 1
-
-def encoder_callback2B(gpio, level, tick):
-    global motor2_encoder_count
-    motor2_encoder_count += 1
 
 def calcular_rps(encoder_count, elapsed_time):
     return ((encoder_count / encoder_counts_per_rev)/ motor_resolution) / elapsed_time
@@ -60,8 +53,7 @@ def main():
     # Configurar callbacks para los flancos del encoder
     pi.callback(motor1_encoder_pinA, pigpio.FALLING_EDGE, encoder_callback1A)
     pi.callback(motor2_encoder_pinA, pigpio.FALLING_EDGE, encoder_callback2A)
-    pi.callback(motor1_encoder_pinB, pigpio.FALLING_EDGE, encoder_callback1B)
-    pi.callback(motor2_encoder_pinB, pigpio.FALLING_EDGE, encoder_callback2B)
+
 
     # Establecer velocidad al 100% para ambos motores
     control_motor(motor1_pwm_pin, motor1_dir_pin, 100, 'forward')
