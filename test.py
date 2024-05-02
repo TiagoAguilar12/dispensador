ef940eb134001a68f114e7d4994c8393e0292b05
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-05-02 17:14:35 -0500

    3321

--- test.py
+++ test.py
@@ -21,7 +21,6 @@ RPS = 0.0
 RPM = 0.0
 tiempo_actual = 0
 
-
 pi = pigpio.pi()
 
 pi.set_mode(PIN_ENCODER_A, pigpio.INPUT)
@@ -84,15 +83,14 @@ def main():
             print('Velocidad motor 1:', motor1_speed)
             print('Velocidad motor 2:', motor2_speed)
             
-            RPS = (numero_flancos_A + numero_flancos_B) / 1216.0
-            RPM = RPS * 60
-            print("Revoluciones por segundo: {:.2f} | Revoluciones por Minuto: {:.2f}".format(RPS, RPM))
-
-            numero_flancos_B = 0
-            numero_flancos_A= 0
-            tiempo_anterior = tiempo_actual
-            time.sleep(1)
-           
+            if tiempo_actual - tiempo_anterior >= INTERVALO:
+                RPS = (numero_flancos_A + numero_flancos_B) / 1216.0
+                RPM = RPS * 60
+                print("Revoluciones segundo: {:.2f} | Revoluciones por Minuto: {:.2f}".format(RPS, RPM))
+                numero_flancos_B = 0
+                numero_flancos_A = 0
+                tiempo_anterior = tiempo_actual
+                time.sleep(INTERVALO)
 
         pi.set_PWM_dutycycle(motor1_pwm_pin, 0)
         pi.set_PWM_dutycycle(motor2_pwm_pin, 0)
@@ -104,3 +102,5 @@ def main():
         print('Tiempo de funcionamiento de los motores completado.')
 
 main()
+
+
