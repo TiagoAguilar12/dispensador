0e8c4dee3d6b3fc205e8829064480edcc4ec91e6
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-05-20 14:47:00 -0500

    P1 M1 Blanco

--- test.py
+++ test.py
@@ -155,7 +155,7 @@ def control_motores_y_medicion():
         start_time = time.time()
 
         # Crear el archivo de salida para guardar los datos
-        output_file_path = '/home/santiago/Documents/dispensador/dispensador/resultadosM2_Blanco.txt'
+        output_file_path = '/home/santiago/Documents/dispensador/dispensador/resultadosM1_Blanco.txt'
         with open(output_file_path, 'w') as output_file:
             output_file.write("Tiempo\tPorcentaje Motor 1\tPorcentaje Motor 2\tPeso (g)\n")
 
