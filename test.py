e2f738fcdb87611abc027b6ed6212d61c2679bb4
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-06-05 11:29:46 -0500

    gtyj

--- test.py
+++ test.py
@@ -148,7 +148,7 @@ def control_motores_y_medicion():
         start_time = time.time()
 
         # Crear el archivo de salida para guardar los datos
-        output_file_path = '/home/santiago/Documents/dispensador/dispensador/resultados2M2_Blanco.txt'
+        output_file_path = '/home/santiago/Documents/dispensador/dispensador/resultados2M2_Cafe.txt'
         with open(output_file_path, 'w') as output_file:
             output_file.write("Tiempo\t PWM \t Velocidad Angular\t RPM \tPeso (g)\t Voltaje \n")
 
