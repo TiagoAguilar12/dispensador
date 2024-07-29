6b4355fd0b7c74e6a1cc88f92169f8341c08f0e5
Santiago Aguilar <santiagoaguilarp12@gmail.com>
2024-07-29 14:16:51 -0500

    PRUEBA 1 LQR MOTOR 1

--- galga.py
+++ galga.py
@@ -1,70 +1,175 @@
-#!/usr/bin/env python3
 # -- coding: utf-8 --
-import RPi.GPIO as GPIO  # importa GPIO
-from hx711 import HX711  # importa la clase HX711
-GPIO.setwarnings(False)  # elimina los warnings
- 
-try:
-    GPIO.setmode(GPIO.BCM)  # Pines GPIO en numeración BCM
-    # Crea un objeto hx que represente el chip HX711 real
-    # Los parámetros de entrada obligatorios son solo 'Pin_Dato' y 'PD_sck'
-    hx = HX711(dout_pin=21, pd_sck_pin=20)
-    # Medir la tara y guardar el valor como compensación para el canal actual
-    # y ganancia seleccionada. Eso significa canal A y ganancia 128
-    err = hx.zero()
-    #
-    # Verifica si todo está correcto
-    if err:
-        raise ValueError('La tara no se puede definir.')
- 
-    reading = hx.get_raw_data_mean()
-    if reading:     # Verificar si el valor correcto 
-                    # ahora el valor está cerca de 0
-        print('Datos restados por compensación pero todavía no convertidos a unidades:',
-              reading)
-    else:
-        print('Dato invalido', reading)
+#!/usr/bin/env python3
+import time
+import pigpio #type: ignore
+import math
+from pytictoc import TicToc #type: ignore
+import numpy as np # type: ignore
+from numpy import array  #type: ignore
+#import serial #type:ignore
 
-    
- 
-    # Para calcular la tasa de conversión a algunas unidades, en en este caso gramos,
-    # Se debe partir de un peso conocido para ajustar.
-    input('Coloque un peso conocido en la balanza y luego presione Enter')
-    reading = hx.get_data_mean()
-    print(reading)    
-    if reading:
-        print('Valor medio de HX711 restado para compensar:', reading)
-        known_weight_grams = input(
-            'Escriba cuántos gramos eran y presiona Enter: ')
-        try:
-            value = float(known_weight_grams)
-            print(value, 'gramos')
-        except ValueError:
-            print('Entero o flotante esperado y tengo:',
-                  known_weight_grams)
- 
-       
-        # establecer la relación de escala para un canal en particular y una ganancia
-        # utilizada para calcular la conversión a unidades. El argumento requerido es solo
-        # una relación de escala. Sin argumentos 'canal' y 'ganancia_A' establece
-        # la relación entre el canal actual y la ganancia.
-        ratio = reading / value   # calcular la relación para el canal A y la ganancia 128
-        hx.set_scale_ratio(ratio) # Determina la proporción para el canal actual
-        print('Relación de peso establecida.')
+# Inicialización de Pigpio
+pi = pigpio.pi()
+pi_m = math.pi 
+
+# Configuración de pines de motor y encoder
+motor1_pwm_pin = 12
+motor1_dir_pin = 24
+motor1_en_pin = 22
+motor2_pwm_pin = 13
+motor2_dir_pin = 25
+motor2_en_pin = 23
+
+PIN_ENCODER_A = 18
+PIN_ENCODER_B = 17
+PIN_ENCODER2_A = 16
+PIN_ENCODER2_B = 19
+
+T = 0.2
+
+# Configuración de pines de entrada para los encoders
+pi.set_mode(PIN_ENCODER_A, pigpio.INPUT)
+pi.set_pull_up_down(PIN_ENCODER_A, pigpio.PUD_UP)
+pi.set_mode(PIN_ENCODER_B, pigpio.INPUT)
+pi.set_pull_up_down(PIN_ENCODER_B, pigpio.PUD_UP)
+
+pi.set_mode(PIN_ENCODER2_A, pigpio.INPUT)
+pi.set_pull_up_down(PIN_ENCODER2_A, pigpio.PUD_UP)
+pi.set_mode(PIN_ENCODER2_B, pigpio.INPUT)
+pi.set_pull_up_down(PIN_ENCODER2_B, pigpio.PUD_UP)
+
+# Funciones de callback para contar flancos
+def contador_flancos_encoder(gpio, level, tick):
+    global numero_flancos_A
+    numero_flancos_A += 1
+
+def contador_flancos_encoder_b(gpio, level, tick):
+    global numero_flancos_B
+    numero_flancos_B += 1
+
+def contador_flancos_encoder2(gpio, level, tick):
+    global numero_flancos_A2
+    numero_flancos_A2 += 1
+
+def contador_flancos_encoder_b2(gpio, level, tick):
+    global numero_flancos_B2
+    numero_flancos_B2 += 1
+
+# Configuración de callbacks
+cb1 = pi.callback(PIN_ENCODER_A, pigpio.EITHER_EDGE, contador_flancos_encoder)
+cb3 = pi.callback(PIN_ENCODER2_A, pigpio.EITHER_EDGE, contador_flancos_encoder2)
+
+# Función para controlar el motor
+def control_motor(pin_pwm, pin_dir, speed_percent, direction):
+    duty_cycle = int(speed_percent * 255 / 100)
+    pi.set_PWM_dutycycle(pin_pwm, duty_cycle)
+
+    if direction == 'forward':
+        pi.write(pin_dir, 1)  # Dirección hacia adelante
+    elif direction == 'backward':
+        pi.write(pin_dir, 0)  # Dirección hacia atrás
     else:
-        raise ValueError('No se puede calcular el valor medio . ERROR', reading)
- 
-    # Leer datos varias veces y devolver el valor medio
-    # restado por compensación y escalado a las 
-    # unidades deseadas. En este caso en gramos.
-    print("Ahora, leeré datos en un bucle infinito. Para salir presione 'CTRL + C'")
-    input('Presione Enter para comenzar a leer')
-    #print('El peso actual en la balanza en gramos es: ')
-    while True:
-        print("El peso actual en gramos es de %.2f" % (hx.get_weight_mean(20)))
- 
-except (KeyboardInterrupt, SystemExit):
-    print('Chau :)')
- 
-finally:
-    GPIO.cleanup()
+        raise ValueError("Dirección no válida. Usa 'forward' o 'backward'.")
+    
+# Contadores de flancos
+numero_flancos_A = 0
+numero_flancos_B = 0
+numero_flancos_A2 = 0
+numero_flancos_B2 = 0
+
+# Matrices A,B,C,D del modelo
+
+A = np.array([[0.894130801660748, 0.145973156319373, 0.066225686842812, 0.067086287188154],
+            [-0.441836100194477, 0.748393868632439, 0.027872957540047, -0.026971019471969],
+            [0.035223234234442, -0.404264770036147, 0.863341393359765, -0.539775953712378],
+            [0.173570457083066, 0.507384555424424, 0.507384555424424, 0.507384555424424]])
+
+B = np.array([[-0.000419785220888],
+              [-0.006447149525435],
+              [-0.003587244670196],
+              [0.020003639959133]])
+
+C = np.array([[-46.886951437617398, -8.778687090880192, 5.312400689483614, -2.660688977805191]])
+
+D = 0 
+
+# Matriz K y L y constante Ki 
+
+K = np.array([[-96.894235555587898, -54.978236188808395, -16.510278445545580, 0.225397518945667]])
+Ki = -0.726725918817694
+L = np.array([[-0.001633156413536],
+              [0.000304322683582],
+              [0.002212041259930],
+              [-0.000008608682074]])
+
+Ao = (A-L@C)
+Bo = np.array([[-0.000419785220888, -0.001633156413536],
+              [-0.006447149525435, 0.000304322683582],
+              [-0.003587244670196, 0.002212041259930],
+              [0.020003639959133, -0.000008608682074]])
+#Definición de ek, rk, flujo y X
+
+ek = 0.0
+ek_1 = 0.0
+ek_int = 0.0
+ek_int_1 = 0.0
+rk = 0.0
+fk = 0.0
+W = 0.0
+uk = 0.0
+set_point_f = 35.0
+set_point_w = 25.0
+delta_w = 0.0
+delta_w_1 = 0.0
+delta_f = 0.0
+delta_f_1 = 0.0
+delta_f_2 = 0.0
+
+xi = np.array([[0],
+               [0],
+               [0],
+               [0]])
+
+xk_stim = np.array([[0], 
+                   [0],
+                   [0],
+                   [0]]) 
+
+# Loop de Control
+start_time = time.time()
+rk = input("Ingrese la referencia:  ")
+# Crear el archivo de salida para guardar los datos
+output_file_path = '/home/santiago/Documents/dispensador/dispensador/test_controlador_ss.txt'
+with open(output_file_path, 'w') as output_file:
+    output_file.write("Tiempo \t PWM \t W \tFlujo \n")
+
+while(time.time()-start_time <= 20):
+       
+    t1 = TicToc()           # Tic
+
+    ek = rk - fk 
+    ek_int = ek_1 + ek_int_1
+    xik = ek_int*Ki
+    uo = np.array([[uk],
+                   [fk]])
+    xk_stim = Ao@xi + Bo@uo
+    uk = -xik-K@xk_stim
+    motor1_speed = uk
+    control_motor(motor1_pwm_pin, motor1_dir_pin, motor1_speed, 'forward')
+
+    #Lectura de Flancos para medir velocidad
+    flancos_totales_1 = numero_flancos_A + numero_flancos_B
+    FPS = flancos_totales_1 / (600.0)
+    W = FPS * ((2 * pi_m) / T)      #Velocidad del motor
+
+    delta_w = W - set_point_w
+    delta_f = fk - set_point_f
+    delta_f = 0.1969*delta_w_1 + 1.359*delta_f_1 -0.581*delta_f_2
+    delta_f_2 = delta_f_1
+    delta_f_1 = delta_f
+    fk = delta_f + set_point_f
+
+    xi = xk_stim
+    ek_1 = ek
+    delta_w_1 = delta_w 
+    
